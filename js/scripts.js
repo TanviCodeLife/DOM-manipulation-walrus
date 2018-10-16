5cd332a984e9a2b47b2e6545798114f67a5f3ee7
Sheila Stephen and Tanvi Garg <sheila@>
2018-10-16 10:28:27 -0700

    use the fadeIn(),fadeOut()function on the walrus image

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-  $(".clickable").click(function(){
-    $("#click-to-hide").toggle();
-    $("#click-to-show").toggle();
+  $("#clickable1").click(function(){
+    // $("#click-to-hide").toggle();
+    // $("#click-to-show").toggle();
+      $("#click-to-hide").fadeIn(5000);
+      $("#click-to-show").fadeOut(5000);
+    // $("#click-to-show").hide();
+
+
+  });
+  $("#clickable2").click(function(){
+    $("#click-to-hide").fadeOut(1000);
+    $("#click-to-show").fadeIn(1000);
+  //   $("#click-to-show").fadeIn(1000);
+  //   $("#click-to-hide").hide();
   });
 });
