c19a986b9c87e138de27712c8a6c10c27762b446
Sheila Stephen and Tanvi Garg <sheila@>
2018-10-16 11:12:59 -0700

    test fadeIn, fadeOut, fadeToggle on clicked item

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
-  $("#clickable1").click(function(){
-    // $("#click-to-hide").toggle();
+  $(".clickable1").click(function(){
+    // toggle
+    //$("#click-to-hide").toggle();
     // $("#click-to-show").toggle();
-      $("#click-to-hide").fadeIn(5000);
-      $("#click-to-show").fadeOut(5000);
-    // $("#click-to-show").hide();
-
+    //fadeIn and fadeOut
+    // $("#click-to-hide").fadeIn(5000);
+    // $("#click-to-show").fadeOut(5000);
+    //fadeToggle
+    $("#click-to-hide").fadeToggle("slow");
+    $("#click-to-show").fadeToggle("slow");
+    //slideUp and slideDown
 
   });
-  $("#clickable2").click(function(){
-    $("#click-to-hide").fadeOut(1000);
-    $("#click-to-show").fadeIn(1000);
+
+  // $("#clickable2").click(function(){
+  //   $("#click-to-hide").fadeOut(1000);
+  //   $("#click-to-show").fadeIn(1000);
   //   $("#click-to-show").fadeIn(1000);
   //   $("#click-to-hide").hide();
-  });
+  //fadeToggle
+  //   $("#click-to-show").fadeToggle(5000);
+  // });
 });
